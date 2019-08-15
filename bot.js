5b5680dbaad24a600815bfb1cb9e5f6094c3fcd7
RaazDiscord <54144604+RaazDiscord@users.noreply.github.com>
2019-08-15 17:37:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -330,4 +330,4 @@ client.on('message', msg => {
    }
    });
 /////
-client.login('NTU4NjMzNzI3MjkxNTU1ODcw.XOtC_A.hKNRUxWxDJCKIPLe6gD_jp1m-NU');
+client.login('NjExNTYyMTg0OTIwNjYyMDI2.XVVn4Q.rIJy2V80yqXzMXzhUaVTebygInk');
